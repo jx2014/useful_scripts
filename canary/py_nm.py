093fff178eb3c8bc7d0218e6655c73199df0dc4a
main_laptop <jx@vmware>
2017-07-28 17:13:49 -0700

    add py_nm

---
+++ canary/py_nm.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python2.7
+#
+# *** script to handle net_mgr io ***
+# 
+# last update:
+# 7/28/2017 first entry
+
+import re
+import subprocess
+import os
+import time
+import logging
+
+NETMGR = r"/home/ssnuser/work/net_mgr"
+uut = 
+
+
