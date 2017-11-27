836111e85c64bfb91555d04b1dff56b03735f669
ssn rma laptop <laptop@laptop>
2017-11-26 19:32:03 -0800

    working version

--- fsu_keep_alive.py
+++ fsu_keep_alive.py
@@ -4,9 +4,36 @@
 # 
 # update:
 # 10/8/2017 first commit
+# 11/26/2017 working version
 
 import pynm
+import subprocess
+from subprocess import PIPE, STDOUT
+import time
+
 
 fsu = pynm.Test(debug=1, timeout=10)
 
-print fsu.CheckFSU()
+def ConnectFSU():
+    fsuConnect = r"/home/ssnuser/work/launchgwnccd.sh"
+    args = [fsuConnect]
+    p = subprocess.Popen(args, shell=True)
+
+def CheckFSUonline():
+    fsu_msg = fsu.CheckFSU()
+    if 'refused' in fsu_msg:
+        return False
+    else:
+        print 'FSU online',
+        return True
+
+if __name__ == '__main__':
+    while True:
+        if CheckFSUonline():
+            print '.',
+        else:
+            print '%s fsu offline, reconnecting..' % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            ConnectFSU()
+        time.sleep(60)
+
+
